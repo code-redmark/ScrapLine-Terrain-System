2b65156374b29d35f1068ce9b0a576f058a38d58
Marco <fallace.marco@gmail.com>
2026-04-17 22:27:06 +0200

    Little improvement in render time by replacing Roblox API's ReadVoxels with "manual" creation of occupancy and material tables for WriteVoxels

--- src/server/TerrainSystem/Rendering/VoxelModule.lua
+++ src/server/TerrainSystem/Rendering/VoxelModule.lua
@@ -24,68 +24,75 @@ function Voxel.RenderChunkTerrain(ChunkPosition: types.Position2D, TerrainMap: {
 	local baseZ = ChunkPosition.z * sizeStuds
 
 	-- IMPORTANT: Y size must be consistent with terrain limits
-	local heightStuds = 64 * CELLSize
+	local heightStuds = variables.HM_AMPLITUDE * 3 + 1
 
 	local region = Region3.new(
 		Vector3.new(baseX, 0, baseZ),
 		Vector3.new(baseX + sizeStuds, heightStuds, baseZ + sizeStuds)
 	)
 
-	local materials, occupancy = EngineTerrain:ReadVoxels(region, resolution)
+	local materials = {}
+	local occupancy = {}
 
-	local sizeX = materials.Size.X
-	local sizeY = materials.Size.Y
-	local sizeZ = materials.Size.Z
-	local air = Enum.Material.Air
+	local sizeX = math.ceil(region.Size.X / resolution)
+	local sizeY = math.ceil(region.Size.Y / resolution)
+	local sizeZ = math.ceil(region.Size.Z / resolution)
 
-	-- fill
+	
 	for x = 1, sizeX do
+		materials[x] = {}
+		occupancy[x] = {}
 		local materialColumn = materials[x]
-		local occupancyColumn = occupancy[x]
+		local occupancyColumn = occupancy[x] 
+
 		for z = 1, sizeZ do
+			
+			local sampleIndex = sizeZ * (x - 1) + z
+			local sample = TerrainMap[sampleIndex]
 
-			local index = (x - 1) * sizeZ + z
-			local sample = TerrainMap[index]
+				if sample then
 
-			if sample then
+					local mat = sample.biome.SurfacePart.Material
+					local h = math.floor(sample.height / resolution)
 
-				local mat = sample.biome.SurfacePart.Material
-				local h = math.floor(sample.height / resolution)
+					if h < 1 then h = 1 end
 
-				if h < 1 then h = 1 end
-				if h > sizeY then h = sizeY end
+					if h > sizeY then print('hit height limit'); h = sizeY end
 
-				for y = 1, h do
-					materialColumn[y][z] = mat
-					occupancyColumn[y][z] = 1
-				end
+					for SolidY = 1, h do
+						materialColumn[SolidY] = materialColumn[SolidY] or {}
+						occupancyColumn[SolidY] = occupancyColumn[SolidY] or {}
+						materialColumn[SolidY][z] = mat
+						occupancyColumn[SolidY][z] = 1
+					end
 
-				for y = h + 1, sizeY do
-					materialColumn[y][z] = air
-					occupancyColumn[y][z] = 0
-				end
-			else
-				-- important: clear column to avoid holes
-				for y = 1, sizeY do
-					materialColumn[y][z] = air
-					occupancyColumn[y][z] = 0
-				end
+					for EmptyY = h + 1, sizeY do
+						materialColumn[EmptyY] = materialColumn[EmptyY] or {}
+						occupancyColumn[EmptyY] = occupancyColumn[EmptyY] or {}
+						materialColumn[EmptyY][z] = Enum.Material.Air
+						occupancyColumn[EmptyY][z] = 0
+					end
+				else
+					-- important: clear column to avoid holes
+					for ClearY = 1, sizeY do
+						materialColumn[ClearY][z] = Enum.Material.Air
+						occupancyColumn[ClearY][z] = 0
+					end
 			end
 
-			if processed >= batchSize then
-				task.wait()
-				processed = 0
-				continue
-			else
-				processed += 1
-			end
+			if processed >= batchSize then task.wait(); processed = 0
+			else processed += 1 end
 
 		end
+
+		
+
 	end
-	
+
 	task.synchronize()
 
-	EngineTerrain:WriteVoxels(region, resolution, materials, occupancy)
+	print(#materials[1])
+	EngineTerrain:WriteVoxels(region:ExpandToGrid(resolution), resolution, materials, occupancy)
 
 	task.desynchronize()
 end
